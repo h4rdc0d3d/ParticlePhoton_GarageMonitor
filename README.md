344022ce28e3f18292c9c9985fa3dbece025c8f0
Kevin Sutton <ksutton30189@gmail.com>
2018-05-06 02:44:24 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# photon_garage-monitor
+# Particle Photon: Garage Monitor & Controller
+
+[![Waffle.io - Columns and their card count](https://badge.waffle.io/kevinsutton/photon_garage-monitor.svg?columns=all)](https://waffle.io/kevinsutton/photon_garage-monitor)
---
+++ src/dist-config.h
@@ -0,0 +1,4 @@
+// This file is used for all basic configuration options needed to comnunicate with 3rd party APIs (i.e. Particle Cloud)
+// This template has been provided in place of the actual configuration file containing credential infomration.  You will need to rename this file to config.h (or change the include contained in the appropriate *.ino files).
+
+// Particle Cloud API Credentials
